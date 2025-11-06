950a0ee697263c66210830fe99284b2b98d94349
Zdenko Abarca <eabarcac004@alumno.uaemex.mx>
2025-11-05 23:17:14 -0600

    Supabase implementation done

---
+++ src/lib/supabase/server.ts
@@ -0,0 +1,32 @@
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+
+export async function createClient() {
+  const cookieStore = (await cookies()) as any
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value, ...options })
+          } catch {
+            // Manejar error si se llama desde un Server Component
+          }
+        },
+        remove(name: string, options: CookieOptions) {
+          try {
+            cookieStore.set({ name, value: '', ...options })
+          } catch {
+            // Manejar error
+          }
+        },
+      },
+    }
+  )
+}
